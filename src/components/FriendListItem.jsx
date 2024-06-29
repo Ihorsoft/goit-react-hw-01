c2d7d589f8da3c1465ac8bfe974d2a291291b001
Ihorsoft <0639492002s@gmail.com>
2024-06-29 16:42:36 +0300

    task1 task1  html

--- src/components/FriendListItem.jsx
+++ src/components/FriendListItem.jsx
@@ -1,3 +1,5 @@
+import { Alertcolor } from "./Alertcolor";
+
 export const FriendListItem = ({ item }) => {
   // export const FriendListItem = ({ avatar, name, isOnline, id }) => {
   // console.log("avatar is :", item.avatar);
@@ -6,7 +8,15 @@ export const FriendListItem = ({ item }) => {
       <img src={item.avatar} alt={item.avatar} width="48" />
       <p>{item.name}</p>
 
-      {item.isOnline ? <p>Online</p> : <p>Offline</p>}
+      {item.isOnline ? (
+        <Alertcolor variant="success">
+          <p>Online</p>
+        </Alertcolor>
+      ) : (
+        <Alertcolor variant="error">
+          <p>Offline</p>
+        </Alertcolor>
+      )}
 
       {/* <img src={avatar} alt={avatar} width="48" />
       <p>{name}</p>
